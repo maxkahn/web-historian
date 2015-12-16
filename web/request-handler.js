308d2720c0f1fc4364b6b3685e34e6f33976fec1
maxkahn <kahn.max@gmail.com>
2015-12-16 15:45:30 -0800

    Passes first 4 unit tests; server runs.

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,66 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+
+  var filename = '';
+
+  if(req.method == 'GET'){
+    if(req.url == '/'){
+      //the ./ makes it an absolute path
+      filename = './web/public/index.html';
+    }
+    else{
+      filename = archive.paths.archivedSites +  req.url;
+    }
+
+    fs.exists(filename, function(exists){
+      console.log(filename);
+      if(exists){
+        fs.readFile(filename, function(err, data){
+          if(err) throw err;
+          res.end(data);
+        });
+      }
+      else{
+        //return 404 error
+        res.writeHead(404);
+        res.end(archive.paths.list);
+      }
+    });
+
+  }
+  else if(req.method == 'POST'){
+    //save the site
+    // fs.appendFile(archive.paths.list, )
+    var body = '';
+    req.on('data', function(data){
+      body += data;
+    });
+    req.on('end', function(){
+      //equal signs in text are interpreted as %3D thus this is valid splitting behaviour
+      var dat = body.toString().split('=');
+      console.log(dat[1]);
+      var dataToAppend = dat[1] + '\n';
+
+      //append to textfile
+      fs.appendFile(archive.paths.list, dataToAppend, function(err){
+        if(err) throw err;
+        console.log('we tried to write!');
+      });
+
+      //return 302
+      res.writeHead(302);
+      res.end();
+
+    });
+
+    
+  }
+
+  
+
+  // res.end(archive.paths.list);
 };
