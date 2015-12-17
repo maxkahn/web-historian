0b7d7bf753cb6d7f5f79a30c185487c5c6b306fb
maxkahn <kahn.max@gmail.com>
2015-12-17 15:30:05 -0800

    Created and executed cron to run every minute. Verified that this works.

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,5 +1,6 @@
 var http = require("http");
 var helpers = require('../web/http-helpers');
+var archive = require('../helpers/archive-helpers');
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
 
@@ -19,30 +20,4 @@ form-encoded data
 */
 //NOT FINAL FORM OF METHOD
 //DO NOT FORGET TO REFACTOR
-var fetchRemoteHTML = function(url){
-  var options = {
-    hostname: 'www.google.com',
-    port: 80,
-    headers: helpers.headers
-  };
-
-  //VOMITS INTO CONSOLE
-  var req = http.request(options, function(res) {
-    console.log('STATUS: ' + res.statusCode);
-    console.log('HEADERS: ' + JSON.stringify(res.headers));
-    res.setEncoding('utf8');
-    res.on('data', function (chunk) {
-      console.log('BODY: ' + chunk);
-    });
-    res.on('end', function() {
-      console.log('No more data in response.');
-    });
-  });
-
-  req.on('error', function(e) {
-    console.log('problem with request: ' + e.message);
-  });
-
-  req.end();
-};
-
+archive.readListOfUrls(archive.downloadUrls);
